7f3dfee8054caf5d5e0f8e9df9c724cb5373d837
ycoll <yascoll@gmail.com>
2020-03-29 13:17:58 +0000

    Fixing Issue

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'nim_reads'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+print(os.getenv('MONGO_URI'))
+
+mongo = PyMongo(app)
+
+
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_reviews')
+def get_reviews():
+    return render_template("reviews.html", reviews=mongo.db.reviews.find())
 
 
-if __name__ == '__main__':   
-    app.run(host=os.environ.get('IP'),
-        port=(os.environ.get('PORT')),
-        debug=True)
+if __name__ == '__main__':  
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=(os.environ.get('PORT', '5000')),
+            debug=True)
