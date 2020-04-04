5d560e3ed6758d5900e1d4af703141cd5ff75fc4
ycoll <yascoll@gmail.com>
2020-04-04 15:40:08 +0000

    Added JS for contact form

---
+++ static/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactMe) {
+	emailjs.send("gmail", "nim-reads", {
+			"from_name": contactMe.name.value,
+			"from_email": contactMe.email.value,
+			"message": contactMe.message.value
+		})
+		.then(
+			resetForm(),
+		);
+	return false;
+
+}
+
+function resetForm() {
+	let form = document.getElementById('myForm');
+	form.reset();
+}
