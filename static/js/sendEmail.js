9e5c883edbd384b2d2c96467fdb40796c16d172d
ycoll <yascoll@gmail.com>
2020-04-05 12:44:20 +0000

    Fixed issue with sidenav

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,4 +1,5 @@
 function sendMail(contactMe) {
+    emailjs.init("user_2XLNWuqeljGFG1tggK7In");
     emailjs.send("gmail", "nim-reads", {
         "from_name": contactMe.name.value,
         "from_email": contactMe.email.value,
